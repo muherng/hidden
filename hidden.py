9ca344af85e2d0248897350d5f92edfa78b76bc3
Morris Yau <morrisyau@align-3.csail.mit.edu>
2025-01-04 12:46:13 -0500

    renamed modfiy to hidden

---
+++ hidden.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+import argparse
+import time
+import math
+import os
+import torch
+import torch.nn as nn
+import torch.onnx   
+
+import data
+import model
+
+parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 RNN/LSTM/GRU/Transformer Language Model')
+parser.add_argument('--data', type=str, default='./data/wikitext-2',
+                    help='location of the data corpus')
+parser.add_argument('--model', type=str, default='LSTM',
+                    help='type of network (RNN_TANH, RNN_RELU, LSTM, GRU, Transformer)')
+parser.add_argument('--emsize', type=int, default=200,
+                    help='size of word embeddings')
+parser.add_argument('--nhid', type=int, default=200,
+                    help='number of hidden units per layer')
+parser.add_argument('--nlayers', type=int, default=2,
+                    help='number of layers')
+parser.add_argument('--lr', type=float, default=20,
+                    help='initial learning rate')
+parser.add_argument('--clip', type=float, default=0.25,
+                    help='gradient clipping')
+parser.add_argument('--epochs', type=int, default=40,
+                    help='upper epoch limit')
+parser.add_argument('--batch_size', type=int, default=20, metavar='N',
+                    help='batch size')
+parser.add_argument('--bptt', type=int, default=35,
+                    help='sequence length')
+parser.add_argument('--dropout', type=float, default=0.2,
+                    help='dropout applied to layers (0 = no dropout)')
+parser.add_argument('--tied', action='store_true',
+                    help='tie the word embedding and softmax weights')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--cuda', action='store_true', default=False,
+                    help='use CUDA')
+parser.add_argument('--mps', action='store_true', default=False,
+                        help='enables macOS GPU training')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                    help='report interval')
+parser.add_argument('--save', type=str, default='model.pt',
+                    help='path to save the final model')
+parser.add_argument('--onnx-export', type=str, default='',
+                    help='path to export the final model in onnx format')
+parser.add_argument('--nhead', type=int, default=2,
+                    help='the number of heads in the encoder/decoder of the transformer model')
+parser.add_argument('--dry-run', action='store_true',
+                    help='verify the code and the model')
+args = parser.parse_args()
+print('args: ', args)
+
+#we need args to import all the functions from main
+from train import batchify, repackage_hidden, get_batch, train, export_onnx
+
+if __name__ == '__main__':
+    # Set the random seed manually for reproducibility.
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        if not args.cuda:
+            print("WARNING: You have a CUDA device, so you should probably run with --cuda.")
+    if hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        if not args.mps:
+            print("WARNING: You have mps device, to enable macOS GPU run with --mps.")
+
+    use_mps = args.mps and torch.backends.mps.is_available()
+    if args.cuda:
+        device = torch.device("cuda")
+    elif use_mps:
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
+    ###############################################################################
+    # Load data
+    ###############################################################################
+
+    corpus = data.Corpus(args.data)
+
+    # Starting from sequential data, batchify arranges the dataset into columns.
+    # For instance, with the alphabet as the sequence and batch size 4, we'd get
+    # ┌ a g m s ┐
+    # │ b h n t │
+    # │ c i o u │
+    # │ d j p v │
+    # │ e k q w │
+    # └ f l r x ┘.
+    # These columns are treated as independent by the model, which means that the
+    # dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
+    # batch processing.
+
+    eval_batch_size = 10
+    train_data = batchify(corpus.train, args.batch_size, device=device)
+    val_data = batchify(corpus.valid, eval_batch_size, device=device)
+    test_data = batchify(corpus.test, eval_batch_size, device=device)
+
+    ###############################################################################
+    # Build the model
+    ###############################################################################
+
+    ntokens = len(corpus.dictionary)
+    if args.model == 'Transformer':
+        model = model.TransformerModel(ntokens, args.emsize, args.nhead, args.nhid, args.nlayers, args.dropout).to(device)
+    else:
+        model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.tied).to(device)
+
+    criterion = nn.NLLLoss()
+
+    ###############################################################################
+
+
+    def evaluate(data_source):
+        # Turn on evaluation mode which disables dropout.
+        model.eval()
+        total_loss = 0.
+        ntokens = len(corpus.dictionary)
+        if args.model != 'Transformer':
+            hidden = model.init_hidden(eval_batch_size)
+            print('hidden: ', hidden) 
+        with torch.no_grad():
+            for i in range(0, data_source.size(0) - 1, args.bptt):
+                data, targets = get_batch(data_source, i, args=args)
+                if args.model == 'Transformer':
+                    output = model(data)
+                    output = output.view(-1, ntokens)
+                else:
+                    
+                    output, hidden = model(data, hidden)
+                    hidden = repackage_hidden(hidden)
+                total_loss += len(data) * criterion(output, targets).item()
+        return total_loss / (len(data_source) - 1)
+
+
+    #when running the model, the model is saved in the saved_models folder
+    # Load the best saved model.
+    file = 'saved_models/' + args.model + '/' + args.save
+    print('file: ', file)
+    with open(file, 'rb') as f:
+        model = torch.load(f)
+        # after load the rnn params are not a continuous chunk of memory
+        # this makes them a continuous chunk, and will speed up forward pass
+        # Currently, only rnn model supports flatten_parameters function.
+        if args.model in ['RNN_TANH', 'RNN_RELU', 'LSTM', 'GRU']:
+            model.rnn.flatten_parameters()
+
+    # Run on test data.
+    test_loss = evaluate(test_data)
+    print('=' * 89)
+    print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+        test_loss, math.exp(test_loss)))
+    print('=' * 89)
+
+    if len(args.onnx_export) > 0:
+        # Export the model in ONNX format.
+        export_onnx(args.onnx_export, batch_size=1, seq_len=args.bptt)
+
