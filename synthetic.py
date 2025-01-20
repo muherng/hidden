764c13363fda90c87907e608280777510b22cc87
Morris Yau <morrisyau@align-3.csail.mit.edu>
2025-01-20 13:21:10 -0500

    strongly suspect there's a bug in collect_hidden_from_tokens, have changed its inputs to random vectors, will next try to change its hidden to init_hidden zeros

--- synthetic.py
+++ synthetic.py
@@ -508,7 +508,7 @@ class LSTM_Dataset(Dataset):
                 for batch, i in enumerate(range(0, train_data.size(0) - 1, seq_len)):
                     input_batch, targets = get_batch(train_data, i, seq_len)
                     data_batch, mask_batch, hidden = model.collect_hidden_from_tokens(hidden, input_batch)
-                    if data_batch.size(0) != seq_len*(2*self.num_layers + 2):
+                    if data_batch.size(0) != seq_len*(2*self.num_layers + 1):
                         print('skipping batch: ', batch)
                         continue
                     if batch == 0: 
