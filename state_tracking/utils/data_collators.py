7b575e87977d56bfe8100092265059bf9db07621
Morris Yau <morrisyau@aia-h200-6.csail.mit.edu>
2026-01-24 14:10:03 -0500

    state tracking with chunk size 2, T1 = 2, T2 = 2, with schedule in chunk_size appears to be working

--- state_tracking/utils/data_collators.py
+++ state_tracking/utils/data_collators.py
@@ -77,6 +77,7 @@ class DataCollatorForLanguageModelingWithDirectSupervision:
                 max_length=self.max_len,
                 return_tensors="pt"
             )["input_ids"]
+            
             # make same shape as input_ids
             batch["labels"] = batch["labels"][:, :batch["input_ids"].size(1)]
             #print('batch["input_ids"]:', batch["input_ids"].shape)
